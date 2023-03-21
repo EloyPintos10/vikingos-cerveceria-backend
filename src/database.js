8c90519828a047cd62c8138e5c81b4bcd25be57d
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2023-03-21 17:54:52 -0300

    buscando solucion de problema cors

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 //localhost = 127.0.0.1
-// const url = 'mongodb://127.0.0.1:27017/ragnar-indumentaria';// local
+ //const url = 'mongodb://127.0.0.1:27017/ragnar-indumentaria';// local
 const url = 'mongodb+srv://eloyPintos10:vikingo10@cluster0.tnezf59.mongodb.net/vikingo-cerveceria';
 
 mongoose.connect(url);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv'
 import express from 'express';
-import cors from 'cors';
+import cors from "cors"
 import morgan from 'morgan';
 import path from 'path';
 import productoRouter from './routes/productos.routes'
@@ -9,14 +9,19 @@ import authRouter from './routes/usuario.routes'
 import './database';
 dotenv.config();
 
+
 const app = express();
+
 
 app.set('port', process.env.PORT || 4000);
 
 app.listen( app.get('port'), ()=>{
     console.log('Estoy en el puerto ' + app.get('port'))
 })
-//middlewears: 
+
+
+//middlewears:  
+
 app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true}));
@@ -28,3 +33,4 @@ app.use(express.static(path.join(__dirname, '../public')))
 app.use('/apiragnar',productoRouter);
 app.use('/apiragnar',pedidosRouter);
 app.use('/apiragnar/auth',authRouter);
+
