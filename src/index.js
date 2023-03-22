a6030a6eef477c5956f2b9b131d06ff7c3bc2844
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2023-03-22 09:23:32 -0300

    cambios en la configuracion del archivo vercel.json

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.listen( app.get('port'), ()=>{
 
 //middlewears:  
 
-app.use(cors({origin: "*"}))
+app.use(cors())
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
